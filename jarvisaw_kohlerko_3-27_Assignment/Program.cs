9e4df00db8f0b92cff8bd6a160f6de4dc0b5390d
Alex Jarvis <awjarvis2004@gmail.com>
2025-03-27 11:48:28 -0400

    Add project files.

---
+++ jarvisaw_kohlerko_3-27_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace jarvisaw_kohlerko_3_27_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseballTicket myBaseballTicket = new BaseballTicket(); // Corrected class name
+            myBaseballTicket.Price = 75.00;  // Invoke the setter
+            Console.WriteLine("Price of my ticket is " + myBaseballTicket.Price); // getter
+        }
+    }
+
+    internal class BaseballTicket // Renamed to match PascalCase
+    {
+        private double _price;
+
+        // Getter and setter for the price property
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
